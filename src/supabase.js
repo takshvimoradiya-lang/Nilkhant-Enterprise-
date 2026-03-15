ce19bec41c75a2ca47d8bae2c9ac5c5dacc31542
takshvimoradiya-lang <takshvimoradiya@gmail.com>
2026-03-15 23:39:20 +0530

    Change Supabase key to a new secret
    Updated supabaseKey to a new secret key.

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://mqzwjuenpvknzvntsmpu.supabase.co";
-const supabaseKey = "sb_publishable_OaCpx_WhO0loeGZlF55k7Q_zdq9Uwr-";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xendqdWVucHZrbnp2bnRzbXB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MjA5MDMsImV4cCI6MjA4OTA5NjkwM30.w80pXOroZLQ1O0bSyVmrc-xHdduAnZRVItkVOmTJOWw";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
