82185cbb2347c3ecd00015a337c58060d7d8bd5c
takshvimoradiya-lang <takshvimoradiya@gmail.com>
2026-03-17 20:29:38 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -3,4 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://mqzwjuenpvknzvntsmpu.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xendqdWVucHZrbnp2bnRzbXB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MjA5MDMsImV4cCI6MjA4OTA5NjkwM30.w80pXOroZLQ1O0bSyVmrc-xHdduAnZRVItkVOmTJOWw";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  realtime: {
+    params: {
+      eventsPerSecond: 10,
+    },
+  },
+});
